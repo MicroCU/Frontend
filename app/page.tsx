9ed31cd797e86d3e4791f2d374f3a68098a555b5
KnotPP <88419066+KnotPP@users.noreply.github.com>
2023-12-22 13:03:46 +0700

    new init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Example } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <Example />
+      <div className="font-inter text-7xl bg-danger">Example</div>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -16,41 +16,6 @@ module.exports = {
       },
     },
     extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -70,7 +35,23 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        "bai-jamjuree": ["Bai Jamjuree", "sans-serif"],
+        "inter": ["Inter", "sans-serif"],
+      },
+      colors: {
+        black: "#000000",
+        grayMain: "#494949",
+        grayMedium: "#7E7D7D",
+        graySmall: "#D9D9D9",
+        grayLight: "#F3F3F3",
+        white: "#FFFFFF",
+        primary: "#5C4EFF",
+        danger: "#FF4E4E",
+        success: "#22C55E",
+      },
     },
+    
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
