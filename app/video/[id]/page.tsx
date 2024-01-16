e792ba5a32593e2db49303e06631f488bd66550f
KnotPP <88419066+KnotPP@users.noreply.github.com>
2024-01-16 16:20:16 +0700

    play/pause handler and progressbar handler

--- app/video/[id]/page.tsx
+++ app/video/[id]/page.tsx
@@ -2,8 +2,9 @@
 
 import VideoControl from "@/components/VideoControl";
 import VideoNav from "@/components/VideoNav";
-import { useEffect, useState } from "react";
-import ReactPlayer from "react-player";
+import { useEffect, useRef, useState } from "react";
+import ReactPlayer, { ReactPlayerProps } from "react-player";
+import { OnProgressProps } from "react-player/base";
 
 interface VideoPageProps {
   params: {
@@ -11,30 +12,80 @@ interface VideoPageProps {
   };
 }
 
+interface VideoState {
+  playing: boolean;
+  muted: boolean;
+  volume: number;
+  played: number;
+  seeking: boolean;
+  buffer: boolean;
+}
+
 const VideoPage = ({ params }: VideoPageProps) => {
   const [isClient, setIsClient] = useState(false);
+
+  const videoPlayerRef = useRef(null);
+
+  const [videoState, setVideoState] = useState<VideoState>({
+    playing: true,
+    muted: false,
+    volume: 0.5,
+    played: 0,
+    seeking: false,
+    buffer: true
+  });
+
+  const { playing, muted, volume, played, seeking, buffer } = videoState;
+
+  const playPauseHandler = () => {
+    setVideoState({ ...videoState, playing: !videoState.playing });
+  };
+
+  const progressHandler = (state: OnProgressProps) => {
+    if (!seeking) {
+      setVideoState({ ...videoState, ...state });
+    }
+  };
+
+  const seekHandler = (value: number[]) => {
+    console.log(value);
+    
+    setVideoState({ ...videoState, played: value[0] / 100 });
+  };
+
+  const seekMouseUpHandler = (value: number[]) => {
+    setVideoState({ ...videoState, seeking: false });
+    (videoPlayerRef.current as ReactPlayer | null)?.seekTo(value[0] / 100);
+  };
 
   useEffect(() => {
     setIsClient(true);
   }, []);
-  return (isClient && (
-    <div className="relative bg-black flex flex-col justify-center items-center w-full h-screen">
-      <ReactPlayer
-        className="p-0 m-0 w-full h-full"
-        url="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
-        // url="https://www.youtube.com/watch?v=KnZmaNZ2Bck"
-        width="100%"
-        height="100%"
-        playing={true}
-        muted={true}
-        controls={false}
-        //   onPlay={handlePlay}
-        //   onPause={handlePause}
-        //   onEnded={handleEnded}
-      />
-      <VideoControl/>
-    </div>
-  ));
+  return (
+    isClient && (
+      <div className="relative bg-black flex flex-col justify-center items-center w-full h-screen">
+        <ReactPlayer
+          ref={videoPlayerRef}
+          className="p-0 m-0 w-full h-full"
+          url="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+          // url="https://www.youtube.com/watch?v=KnZmaNZ2Bck"
+          width="100%"
+          height="100%"
+          playing={playing}
+          muted={true}
+          controls={false}
+          onProgress={progressHandler}
+        />
+        <VideoControl
+          onPlayPause={playPauseHandler}
+          playing={playing}
+          played={played}
+          onSeek={seekHandler}
+          onSeekMouseUp={seekMouseUpHandler}
+        />
+      </div>
+    )
+  );
 };
 
 export default VideoPage;
--- components/VideoControl.tsx
+++ components/VideoControl.tsx
@@ -1,21 +1,55 @@
-import { Play, SkipForward, Volume2 } from "lucide-react";
+import { Maximize, Pause, Play, SkipForward, Volume2 } from "lucide-react";
 import VideoNav from "./VideoNav";
 import { Slider } from "@/components/ui/slider";
 
-const VideoControl = () => {
+interface VideoControlProps {
+  onPlayPause: () => void;
+  playing: boolean;
+  played: number;
+  onSeek: (value: number[]) => void;
+  onSeekMouseUp: (value: number[]) => void;
+}
+
+const VideoControl = ({
+  onPlayPause,
+  playing,
+  played,
+  onSeek,
+  onSeekMouseUp,
+}: VideoControlProps) => {
   return (
     <div className="absolute top-0 left-0 flex-col z-10 flex justify-between w-full h-full">
       <VideoNav videoName={"Example"} />
       <div>mid</div>
       <div className="px-4">
-        <Slider variant="video" defaultValue={[0]} max={100} step={1} />
+        <Slider
+          variant="video"
+          defaultValue={[0]}
+          max={100}
+          step={0.01}
+          value={[played * 100]}
+          onValueChange={onSeek}
+          onValueCommit={onSeekMouseUp}
+        />
         <div className="flex items-center justify-between py-4 px-6">
           <div className="flex items-center w-1/2 gap-5">
-            <Play color="white" />
+            <div className="cursor-pointer" onClick={onPlayPause}>
+              {playing ? <Pause color="white" /> : <Play color="white" />}
+            </div>
             <SkipForward color="white" />
             <Volume2 color="white" />
-            <Slider variant="volume" defaultValue={[100]} max={100} step={1} className="w-20"/>
+            <Slider
+              variant="volume"
+              defaultValue={[100]}
+              max={100}
+              step={1}
+              className="w-20"
+            />
             <p className="text-white Medium16">2.01 / 5.34</p>
+          </div>
+          <div className="flex justify-end items-center w-1/2 gap-5">
+            <p className="text-white Medium16">1x</p>
+            <Maximize color="white" />
           </div>
         </div>
       </div>
