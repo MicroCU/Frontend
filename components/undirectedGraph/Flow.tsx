08baebddd5f4f0783e18f6df85dc0b2295a645a6
Punnapob Jindakul <punnapob.jdk@gmail.com>
2024-03-18 07:24:31 +0700

    design lottie and add in app

--- components/undirectedGraph/Flow.tsx
+++ components/undirectedGraph/Flow.tsx
@@ -3,8 +3,8 @@ import { ReactFlowProvider } from "reactflow";
 import OverviewFlow from "./Graph";
 import { useJourneyGraph } from "@/context/JourneysGraph";
 import { generateInitialNodeEdge } from "@/lib/undirected-nodes-edges";
-import LoadingGraph from "../LoadingGraph";
 import { MenuTab } from "@/types/enum";
+import { Player as Lottie } from "@lottiefiles/react-lottie-player";
 
 export default function Flow() {
   const { journeys, selectedTab, searchKeyword } = useJourneyGraph();
@@ -18,7 +18,7 @@ export default function Flow() {
   ) {
     return (
       <div className="flex flex-1 justify-center items-center">
-        <LoadingGraph />
+        <Lottie autoplay loop src="/lottie/graph.json" />
       </div>
     );
   }
